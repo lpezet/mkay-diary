4e043e09f637b7ca4bb4a999eb747edc76dcfca7
lpezet <lpezet@gmail.com>
2021-06-27 00:24:36 -0400

    One more test for Config command.

--- src/test/commands/config.spec.ts
+++ src/test/commands/config.spec.ts
@@ -1,10 +1,11 @@
 import { assert } from "chai";
 import program from "commander";
-import { configureLogger } from "../../main/logger";
 import { ConfigConstants } from "../../main/config";
 import { ConfigCommand, Deps, Question } from "../../main/commands/config";
 import { InMemoryConfig } from "../config.spec";
 
+/*
+import { configureLogger } from "../../main/logger";
 configureLogger({
   appenders: {
     console: { type: "console", layout: { type: "colored" } },
@@ -13,7 +14,7 @@ configureLogger({
     default: { appenders: ["console"], level: "all" },
   },
 });
-
+*/
 /*
 const inspect = (obj: any, depth: number): void => {
   console.error(util.inspect(obj, false, depth || 5, true));
@@ -41,6 +42,37 @@ describe("command:config", function () {
           : done(new Error("Command failed to register or missing."));
       })
       .catch(done);
+  });
+  it("no_values", function (done) {
+    const testDeps: Deps = {
+      prompt(_questions: Question[]): Promise<any> {
+        _questions.forEach((q) => {
+          switch (q.name) {
+            case "editor":
+              assert.isUndefined(q.default());
+              break;
+            case "hint":
+              assert.isUndefined(q.default());
+              break;
+            case "include_header":
+              assert.isUndefined(q.default());
+          }
+        });
+        return Promise.resolve({});
+      },
+    };
+    const testConfig = new InMemoryConfig();
+    const command = new ConfigCommand(testConfig, testDeps);
+    try {
+      command.execute().then(() => {
+        assert.isUndefined(testConfig.editor());
+        assert.isUndefined(testConfig.includeHeader());
+        assert.isUndefined(testConfig.hint());
+        done();
+      });
+    } catch (err) {
+      done(err);
+    }
   });
   it("basic", function (done) {
     const expected: any = {};
